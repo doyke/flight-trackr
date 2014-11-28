c6e74db4b04e63698e343b04cdf66e73bcd2dfac
eftov <b.fovet@gmail.com>
2014-11-29 00:33:39 +0100

    dsp et teb faits, teb a verifier

--- ADSB_part1_1.m
+++ ADSB_part1_1.m
@@ -1,67 +1,64 @@
 clear all;
 close all;
-
 %% Initialisation des variables
-
-F_e = 20*10^6; % fr?quence d'?chantillonage ? 20 MHz
-T_e = 1/F_e; 
-D_s = 10^6; % d?bit symbole ? 1 MHz
-T_s = 1/D_s; % p?riode symbole ? 1 ?s
-F_se = T_s/T_e; % facteur de sur-?chantillonage = 20
-N_fft = 512; % nombre de points utilis?s pour la FFT
-N_bits = 1000; % nombre de bits du message transmis
-f = ([0:N_fft-1]/N_fft-0.5)*F_e; % axe des fr?quences
+f_e = 20E6;                                     % fréquence d'échantillonnage 20MHz
+T_e = 1/f_e;
+D_s = 1E6;                                      % débit symbole 1MHz
+T_s = 1/D_s;
+f_se = T_s/T_e;
+N_fft = 512;                                    % nombre de points pour la FFT
+N_bits = 1000;                                  % nombre de bits du message transmis
+f = ([0:N_fft-1]/N_fft - 0.5)*f_e;              % axe des fréquences
 
 %% Question 11
+b_k = randi([0 1], 1, N_bits);                  % génération de la séquence binaire
+s_b = b_k;
 
-% G?n?ration de la s?quence binaire
-b_k = randi([0 1], 1, N_bits);
+p = [-ones(1,f_se/2) ones(1,f_se/2)]/2;         % filtre de mise en forme
 
-% Filtre de mise en forme
-p = [-0.5*ones(1, 0.5*T_s*F_e), 0.5*ones(1, 0.5*T_s*F_e)];
+A_k = pammod(b_k, 2);                           % association bit->symbole
+s_s = upsample(A_k, f_se);                      % sur-échantillonnage au rythme T_e
 
-% Association bit symbole
-Ak = 2*b_k-1; % Ak = -1 pour b_k = 0, 1 pour b_k = 1
-
-% Sur-?chantillonage
-Ak = upsample(Ak, F_se);
-
-% Signal s_l(t)
-s_l = 0.5 + conv(Ak, p);
-
-% Affichage des 25 premiers bits du signal s_l
-figure, plot(s_l(1:25*F_se));
+s_l = 1/2 + conv(s_s, p, 'same');               % signal s_l(t)
+figure, plot(s_l(1:25*f_se));                   % affichage des 25 premiers bits
 
 %% Question 12
-
-% Diagramme de l'oeil de dur?e 2*T_s pour les 100 premiers bits envoy?s
-eyediagram(s_l(1:100*F_se), 3*F_se, 2*T_s);
+eyediagram(s_l(1:100*f_se), 2*f_se, 2*T_s);    % diagramme de l'oeil de durée 2*T_s
 
 %% Question 13
-
-% DSP de s_l(t)
 s_l_tmp = s_l;
-% on rajoute des z?ros si la longueur de s_l n'est pas divisible par N
-if length(s_l_tmp)/N_fft-fix(length(s_l_tmp)/N_fft) ~= 0
-    s_l_tmp = [zeros(1,(fix(length(s_l_tmp)/N_fft)+1)*N_fft-length(s_l_tmp)) s_l_tmp];
+% rajout de zéros si la longueur de s_l non divisible par N
+if (mod(length(s_l_tmp), N_fft) ~= 0)
+    s_l_tmp = [zeros(1,N_fft-mod(length(s_l_tmp), N_fft)) s_l_tmp];
 end
-S_l_cut = zeros(0);
-% on calcule la TF puis DSP de chaque sous-vecteur
-for i=1:fix(length(s_l_tmp)/N_fft)
-    tmp = abs(fft(s_l_tmp((i-1)*N_fft+1:i*N_fft), N_fft)).^2;
-    S_l_cut = [S_l_cut; tmp];
+
+n = length(s_l_tmp)/N_fft;
+S_l_cut = zeros(n, N_fft);
+
+% calcul de la TF et DSP de chaque sous-vecteur
+for i = 1:n
+    S_l_cut(i,:) = abs(fft(s_l_tmp((i-1)*N_fft+1:i*N_fft), N_fft)).^2;
 end
+
+% DSP pratique
+DSP_S_l = (1/n) * sum(S_l_cut);
+
+% DSP théorique
 Dirac = [zeros(1,length(f)/2) 1 zeros(1,length(f)/2-1)];
-DSP_S_l_th = Dirac/4 + ((pi^2.*f.^2.*T_s.^3)/(16*sqrt(N_fft))).*sinc(f.*T_s/2).^4; % N_fft au lieu de diviser par 16 = magouille
-DSP_S_l_th_norme = DSP_S_l_th./sqrt(sum(DSP_S_l_th.^2));
-DSP_S_l = (1/(size(S_l_cut,1)))*sum(S_l_cut);
-% finalement, on trace les DSP normalis?es
+DSP_S_l_th = Dirac/4 + (pi^2*T_s^3*f.^2)/(16*N_fft*T_e).*sinc(T_s*f/2).^4;
+
 figure;
 hold on
-plot(f,fftshift(DSP_S_l)./sqrt(sum(DSP_S_l.^2)));
-plot(f,DSP_S_l_th_norme,'r');
-xlim([-F_e/2 F_e/2-F_e/N_fft]);
-legend('DSP r?elle', 'DSP th?orique');
+
+% tracé de la DSP pratique et normalisée
+plot(f, fftshift(DSP_S_l)./sqrt(sum(DSP_S_l.^2)));
+
+% tracé de la DSP théorique et normalisée
+plot(f, DSP_S_l_th./sqrt(sum(DSP_S_l_th.^2)), 'r');
+
+xlim([-f_e/2 f_e/2-f_e/N_fft]);
+legend('DSP réelle', 'DSP théorique');
 title('DSP de s_l(t)');
-xlabel('Fr?quence (Hz)');
+xlabel('Fréquence (Hz)');
 ylabel('Amplitude');
+hold off
--- ADSB_part1_2.m
+++ ADSB_part1_2.m
@@ -2,58 +2,78 @@ clear all;
 close all;
 
 %% Initialisation des variables
+f_e = 20E6;                                     % fréquence d'échantillonnage 20MHz
+T_e = 1/f_e;
+D_s = 1E6;                                      % débit symbole 1MHz
+T_s = 1/D_s;
+f_se = T_s/T_e;
+N_fft = 512;                                    % nombre de points pour la FFT
+N_bits = 1000;                                  % nombre de bits du message transmis
+f = ([0:N_fft-1]/N_fft - 0.5)*f_e;              % axe des fréquences
 
-F_e = 20*10^6; % fr?quence d'?chantillonage ? 20 MHz
-T_e = 1/F_e; 
-D_s = 10^6; % d?bit symbole ? 1 MHz
-T_s = 1/D_s; % p?riode symbole ? 1 ?s
-F_se = T_s/T_e; % facteur de sur-?chantillonage = 20
-N_fft = 512; % nombre de points utilis?s pour la FFT
-N_bits = 112; % nombre de bits du message transmis
-f = ([0:N_fft-1]/N_fft-0.5)*F_e; % axe des fr?quences
+%% Calculs de la question 11
+b_k = randi([0 1], 1, N_bits);                  % génération de la séquence binaire
+s_b = b_k;
 
-% G?n?ration de la s?quence binaire
-b_k = randi([0 1], 1, N_bits);
+p = [-ones(1,f_se/2) ones(1,f_se/2)]/2;         % filtre de mise en forme
 
-% Filtre de mise en forme
-p = [-0.5*ones(1, 0.5*T_s*F_e), 0.5*ones(1, 0.5*T_s*F_e)];
+A_k = pammod(b_k, 2);                           % association bit->symbole
+s_s = upsample(A_k, f_se);                      % sur-échantillonnage au rythme T_e
 
-% Association bit symbole
-Ak = 2*b_k-1; % Ak = -1 pour b_k = 0, 1 pour b_k = 1
-
-% Sur-?chantillonage
-Ak = upsample(Ak, F_se);
-
-% Signal s_l(t)
-s_l = 0.5 + conv(Ak, p);
+s_l = 1/2 + conv(s_s, p, 'same');               % signal s_l(t)
 
 %% Question 14
 
-% Filtre adapt? p_0*(-t)
-p_0 = [];
+% Canal
+y_l = s_l;
+
+% Decodage
+y_l = y_l - 1/2;
+
+% Filtre adapte
+p_adapte = [ones(1,f_se/2) -ones(1,f_se/2)]/2;
+r = conv(y_l, p_adapte, 'same');
+
+% Sous-echantillonage
+r = downsample(r, f_se);
+
+% Decision
+b_k_hat = r;
+b_k_hat(b_k_hat > 0) = 1;
+b_k_hat(b_k_hat < 0) = 0;
 
 % Evolution du TEB en fonction de E_b/N_0
 P_b = zeros(0);
 
-for i=0:10
+for i = 1:10
     b_k = randi([0 1], 1, N_bits);
-    Ak = upsample(Ak, F_se);
-    s_l = 0.5 + conv(Ak, p);
-    h_l = 1;
+    s_b = b_k;
+    A_k = pammod(b_k, 2);
+    s_s = upsample(A_k, f_se);
+    s_l = 1/2 + conv(s_s, p, 'same');
+    
     sigma_n_l = 1/(2*(10.^(i/10)));
     n_l = sqrt(sigma_n_l)*randn(1,length(s_l));
-    y_l = conv(s_l, h_l) + n_l;
-    y_l = [zeros(1,F_se) y_l];
+    y_l = s_l + n_l;
+    y_l = y_l - 1/2;
     
+    r = conv(y_l, p_adapte, 'same');
+    r = downsample(r, f_se);
+    
+    b_k_hat = r;
+    b_k_hat(b_k_hat > 0) = 1;
+    b_k_hat(b_k_hat < 0) = 0;
+    
+    P_b = [P_b sum(abs(b_k_hat-b_k))/N_bits];
 end
 
-%figure;
-%EbN0 = 0:1:10;
-%semilogy(EbN0,P_b);
-%hold on
-%semilogy(EbN0,1/2*erfc(sqrt(10.^(EbN0/10))),'r');
-%hold off
-%title('Evolution du TEB et de la probabilit? d''erreur binaire th?orique');
-%legend('TEB','Probabilit? d''erreur binaire th?orique');
-%xlabel('(E_b/N_0)_{dB}');
-%ylabel('TEB');
+figure;
+EbN0 = 0:9;
+semilogy(EbN0,P_b);
+hold on
+semilogy(EbN0,1/2*erfc(sqrt(10.^(EbN0/10))),'r');
+hold off
+title('Evolution du TEB et de la probabilite d''erreur binaire theorique');
+legend('TEB','Probabilite d''erreur binaire theorique');
+xlabel('(E_b/N_0)_{dB}');
+ylabel('TEB');
