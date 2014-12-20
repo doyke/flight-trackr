efa6111c4ceb3cfda5527bc5c94e34f91232d652
eftov <b.fovet@gmail.com>
2014-12-20 20:43:10 +0100

    script de test SUCCESS

--- testDB.m
+++ testDB.m
@@ -1,3 +1,12 @@
 URL='jdbc:sqlite:/Users/Benjamin/Desktop/Git/GitHub/ADSB/PlaneInfo.db';
 conn = database('','','','org.sqlite.JDBC',URL);
 
+address = '393324';
+
+sqlquery = ['select immat from immatriculation where address = ' address];
+
+curs = exec(conn,sqlquery);
+curs = fetch(curs);
+curs.Data
+
+close(conn);
